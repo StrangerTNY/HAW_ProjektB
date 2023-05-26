15d8d9b7b11ef6fea0237268ec4756c6369cede1
lbluem <lukas.buesen@gmail.com>
2023-05-26 11:54:37 +0200

    Menü Anfang
    Start und Quit funktionieren.
    Main Szene wurde auf das Menü verändert.

---
+++ src/scenes/menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$VBoxContainer/StartButton.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://src/scenes/test_level.tscn")
+
+
+func _on_control_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
